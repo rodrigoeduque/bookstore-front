c46ae2c34c5498b72f969da4ad8611d981c3ba33
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2021-11-17 18:14:56 -0300

    feat : views-home-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,20 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
 
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
+import { HomeComponent } from "./components/views/home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,6 +32,7 @@ import { NavComponent } from "./components/template/nav/nav.component";
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
