d8163cedb669fafa96f8193d54cddb91bfc2c16b
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2021-11-17 17:31:22 -0300

    feat: nav-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,23 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./components/template/header/header.component";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from './components/template/footer/footer.component';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from "./components/template/nav/nav.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
