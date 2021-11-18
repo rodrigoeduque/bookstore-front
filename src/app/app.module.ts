e0780dcdc5bc82f3097fdbd58da81c7a65e0aa71
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2021-11-18 08:22:41 -0300

    feat: categoria-retorno implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -37,6 +38,7 @@ import { CategoriaComponent } from "./components/views/categoria/categoria.compo
     MatIconModule,
     MatCardModule,
     MatTableModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/views/categoria/categoria.component.ts
+++ src/app/components/views/categoria/categoria.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Categoria } from "./categoria.model";
+import { CategoriaService } from "./categoria.service";
 
 @Component({
   selector: "app-categoria",
@@ -6,8 +8,20 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./categoria.component.css"],
 })
 export class CategoriaComponent implements OnInit {
-  constructor() {}
+  categorias: Categoria[] = [];
 
-  ngOnInit(): void {}
   displayedColumns: string[] = ["id", "nome", "descricao", "acoes"];
+
+  constructor(private service: CategoriaService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe((resposta) => {
+      console.log(resposta)
+      this.categorias = resposta;
+    });
+  }
 }
